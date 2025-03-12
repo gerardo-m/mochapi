ba8601ec0363baa4b3f05c9ff87f8fd32adaaa75
Gerardo Miranda <gerardo.miranda.cespedes@gmail.com>
2025-03-11 23:06:21 -0400

    Implemented changing expression form depending on the operation

--- app/controllers/expressions_controller.rb
+++ app/controllers/expressions_controller.rb
@@ -1,7 +1,20 @@
 class ExpressionsController < ApplicationController
+  before_action :set_expression, only: %i[  edit update  ]
+
   def edit
   end
 
   def update
+    @expression.update(expression_params)
+  end
+
+  private
+
+  def set_expression
+    @expression = Expression.find(params.expect(:id))
+  end
+
+  def expression_params
+    params.expect(expression: [ :operation ])
   end
 end
--- lib/constants.rb
+++ lib/constants.rb
@@ -1,5 +1,7 @@
 module Constants
   VERSION="v0.2.0"
   SUPPORTED_METHODS=[ "GET", "POST", "PATCH", "PUT" ]
-  EXPRESSION_OPERANDS=[ "IS_A", "IS_EQUAL", "IS_NOT_EQUAL" ]
+  EXPRESSION_OPERANDS={ "IS_A"=>"type", "IS_NOT_A"=>"type",
+    "IS_PRESENT"=>"presence", "IS_NOT_PRESENT"=> "presence"
+  }
 end
