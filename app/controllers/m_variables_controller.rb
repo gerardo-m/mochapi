cfdf55777779f1e5aacf296854936114283d51ab
Gerardo Miranda <gerardo.miranda.cespedes@gmail.com>
2025-07-16 23:46:54 -0400

    Starting functionality to save variables

--- app/controllers/m_variables_controller.rb
+++ app/controllers/m_variables_controller.rb
@@ -1,11 +1,26 @@
 class MVariablesController < ApplicationController
   # before_action :set_project, only: %i[ show edit update destroy ]
+  before_action :set_m_variable, only: %i[ edit update destroy]
 
   def new
-    @m_variable = MVariable.new
-    @m_variable.name = "Prueba"
+    @m_variable = MVariable.create_default(params.expect(:variable_holder_type), params.expect(:variable_holder_id))
+    @m_variable.save!
   end
 
   def edit
   end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  protected 
+
+  def set_m_variable
+    puts "CALLED"
+    @m_variable = MVariable.find(params.expect(:id))
+  end
+
 end
--- app/models/m_variable.rb
+++ app/models/m_variable.rb
@@ -1,3 +1,32 @@
 class MVariable < ApplicationRecord
+  include NameSequenciable
+
+  attr_accessor :value
   belongs_to :variable_holder, polymorphic: true
+  before_save :assign_value
+
+  VALUE_TYPES = ["Integer", "Text", "Decimal"]
+
+  def self.create_default(variable_holder_type, variable_holder_id)
+    new_m_variable = MVariable.new
+    new_m_variable.name = next_titleized_name(:name)
+    new_m_variable.value_type = "Text"
+    new_m_variable.text_value = "Value"
+    new_m_variable.variable_holder_type = variable_holder_type
+    new_m_variable.variable_holder_id = variable_holder_id
+    new_m_variable
+  end
+
+  protected
+
+  def assign_value
+    if self.value_type == "Integer"
+      self.int_value = self.value
+    elsif self.value_type == "Text"
+      self.text_value = self.value
+    elsif self.value_type == "Decimal"
+      self.decimal_value = self.value
+    end
+  end
+  
 end
